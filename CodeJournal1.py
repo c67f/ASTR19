42d8a656ceb566629a30fd18830df1253f8c5d2d
c67f <caleb.friedman@gmail.com>
2024-10-01 17:25:11 -0700

    Code Journal (actually a python file this time)

---
+++ CodeJournal1.py
@@ -0,0 +1,8 @@
+def PrintStuff():
+	print("Hi! I'm Cal Friedman, he/him. One of my favorite movies is Across the Spiderverse. One of my favorite foods is tuna casserole")
+
+def main():
+	PrintStuff()
+
+if __name__=="__main__":
+	main()
