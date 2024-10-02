eab6c57392a0c5693e0171b5b6efa0654965819e
c67f <caleb.friedman@gmail.com>
2024-10-01 17:26:43 -0700

    Add files via upload

--- CodeJournal1.py
+++ CodeJournal1.py
@@ -1,5 +1,5 @@
 def PrintStuff():
-	print("Hi! I'm Cal Friedman, he/him. One of my favorite movies is Across the Spiderverse. One of my favorite foods is tuna casserole")
+	print("Hi! I'm Cal Friedman, he/him. One of my favorite movies is Across the Spiderverse. One of my favorite foods is tuna casserole.")
 
 def main():
 	PrintStuff()
